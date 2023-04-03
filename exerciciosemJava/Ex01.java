9f1250926f441dcff52d16714517592795e17124
Dougkid33 <douglasaugustomsilva@hotmail.com>
2023-04-03 13:44:50 -0300

    commit inicial

---
+++ exerciciosemJava/Ex01.java
@@ -0,0 +1,16 @@
+package exerciciosemJava;
+
+public class Ex01 {
+	public static void main(String[] args) {
+		int indice = 13;
+		int soma = 0, k =0;
+		
+		while(k < indice) {
+			k++;
+			soma = soma +k;
+			
+		}
+		System.out.println(" O valor da soma: " +soma+ " .");
+		
+	}
+}
